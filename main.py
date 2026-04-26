687f04dd18d2557ad25f3a2acfaea21c201b53ef
Andrea Santamaria <santamariaandrea048@gmail.com>
2026-04-26 14:32:03 -0500

    3

--- main.py
+++ main.py
@@ -11,16 +11,43 @@ def main():
 
         file_path = sys.argv[1]
 
-        print("Command-line arguments:")
-        for arg in sys.argv[1:]:
-            print(arg)
+        if len(sys.argv) < 3:
+            return
 
-        print("\nTasks:")
+        command = sys.argv[2]
         tasks = read_todo_file(file_path)
-        for task in tasks:
-            print(task)
 
-    except IndexError as e:
+        if command == "view":
+            print("Tasks:")
+            for task in tasks:
+                print(task)
+
+        elif command == "add":
+            if len(sys.argv) < 4:
+                raise IndexError('Task description required for "add".')
+
+            task = sys.argv[3]
+            tasks.append(task)
+            write_todo_file(file_path, tasks)
+            print(f'Task "{task}" added.')
+
+        elif command == "remove":
+            if len(sys.argv) < 4:
+                raise IndexError('Task description required for "remove".')
+
+            task = sys.argv[3]
+
+            try:
+                tasks.remove(task)
+                write_todo_file(file_path, tasks)
+                print(f'Task "{task}" removed.')
+            except ValueError:
+                print(f'Task "{task}" not found.')
+
+        else:
+            raise ValueError("Command not found!")
+
+    except (IndexError, ValueError) as e:
         print(e)
 
 if __name__ == "__main__":
